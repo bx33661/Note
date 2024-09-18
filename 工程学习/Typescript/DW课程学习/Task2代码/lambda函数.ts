b03f6290fe62d53fbe5ee99d6e142d779ce46878
bx33661 <bx33661@gmail.com>
2024-09-18 20:47:30 +0800

    笔记更新

---
+++ 工程学习/Typescript/DW课程学习/Task2代码/lambda函数.ts
@@ -0,0 +1,15 @@
+/*
+let greet = (name: string) => `Hello, ${name}`;
+console.log(greet("bx33661"));
+
+let add = (a: number, b: number) => a + b;
+console.log(add(3, 9));
+
+let sayHello = () => console.log("Hello, bx33661!");
+sayHello();
+*/
+let multiply = (a: number, b: number) => {
+    let result = a * b;
+    return result;
+};
+console.log(multiply(9, 3))
