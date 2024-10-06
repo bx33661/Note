467cce3717ee685120ea579133c1727242025584
bx33661 <bx33661@gmail.com>
2024-10-06 11:21:37 +0800

    笔记更新

---
+++ 工程学习/Go/ceshi/helloworld.go
@@ -0,0 +1,14 @@
+// 告诉系统当前编写代码属于哪个包
+package main
+
+import "fmt"
+
+var GBB int = 3333
+
+// 定义一个叫main的函数
+func main() {
+	num := 40
+	fmt.Println(num)
+	fmt.Println(GBB)
+	fmt.Println("hello world")
+}
---
+++ 工程学习/Go/ceshi/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fullName := "John Doe \t(alias \"Foo\")\n"
+	fmt.Println(fullName)
+}
